41a881f7d3022e32f3c82fc61090973520c6e904
JPMoore27 <jp_moore@tamu.edu>
2024-10-23 12:20:34 -0500

    test for 4 threads

---
+++ threading_template/threads_test.py
@@ -0,0 +1,17 @@
+from time import sleep
+import threading
+from queue import Queue
+
+def printstuff(stuff):
+    while True:
+        print(stuff)
+
+if __name__ == "__main__":
+
+
+    #create threads
+    threads = [threading.Thread(target=printstuff, args=(i, )) for i in range(4)]
+
+    #start threads
+    for t in threads:
+        t.start()
