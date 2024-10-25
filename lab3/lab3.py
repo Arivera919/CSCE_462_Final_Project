279b33c0afe91522f75a1e672ec324529e759aa6
JPMoore27 <jp_moore@tamu.edu>
2024-10-25 12:17:37 -0500

    target drivers

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,38 +1,117 @@
-import board
-import busio
-import adafruit_mpu6050
+#Heavily inspired by https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/script
+
+def fuzzycompare(a, b, tolerance):
+    return a + tolerance > b and a - tolerance < b 
+
 import numpy as np
-from os import system
+import os
+from time import sleep 
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
-from time import sleep, perf_counter
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-i2c = busio.I2C(board.SCL, board.SDA)
-mpu = adafruit_mpu6050.MPU6050(i2c)
-steps = 0
-stepUp = False
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 1
+chan1 = AnalogIn(mcp, MCP.P1)
+
+wave = "" #tri, sin, square
+prelimWave = "" #require a wave to be detected twice before being outputted - stops weirdness when the input changes
+tolerance = .1 #tolerance for comparisons in volts
+freq = 0
+
 
 while True:
-    #print("Acceleration X: %.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    #print("%.2f" % (np.abs(mpu.acceleration[0])+np.abs(mpu.acceleration[1])+np.abs(mpu.acceleration[2])))
-    #print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (mpu.gyro))
+    #take some samples
+    vals = []
+    nozeroes = []
+    t = 0
+    #chan0.value chan0.voltage
+    while t < 2000:
+        val = chan1.voltage
+        vals.append(val)
+        if val != 0:
+            nozeroes.append(val)
+        t += 1
+        sleep(.0001)
+
+
+    #categorize wave
+    newWave = ""
+    #check for no signal
+    if fuzzycompare(0, np.mean(vals), .1):
+        newWave = "No signal"
+
     try:
-        z_a = mpu.acceleration[2]
+        dydx = np.gradient(nozeroes)
     except:
-        sleep(.5)
-        i2c = busio.I2C(board.SCL, board.SDA)
-        mpu = adafruit_mpu6050.MPU6050(i2c)
+        print("")
 
-    if(z_a < 7.9 and stepUp):
-        stepUp = False
-        steps += 1
-        sleep(.3)
+    #check for square - many of dydx values are zero
+    dydx_zeroes = np.mean([fuzzycompare(np.abs(dydx[i]),0, .003) for i in range(len(dydx)) ]) 
+    if(dydx_zeroes > .1) and newWave == "":
+        newWave = "Square"
 
-    if(z_a > 9.7):
-        stepUp = True
 
-    print(z_a)
-    print(steps)
+    dydx_var = np.var(np.abs(dydx))
+    #check for tri - lets say at least 75% of derivative values are the same. this method was too vulnerable to noise
+    #dydx_same = np.mean([fuzzycompare(np.abs(dydx[i]), dydx[i+1], .004) for i in range(len(dydx) - 1) ])
+    #if(dydx_same > 0.75) and newWave == "":
+    var_threshold = .000006025 * freq**2 / 2 #constant is gotten from a quadratic regression of measured variance for sin waves. the /2 adds some wiggle room
+    if(dydx_var < var_threshold) and newWave == "":
+        newWave = "Triangle"
+    #getting a signal but it's squiggly - call it a sin
+    if newWave == "":
+        newWave = "Sin"
 
-    sleep(0.01)
 
-    system("clear")
+    #this stops the wave reading from bouncing around when the waves change
+    if wave != newWave:
+        if(newWave != prelimWave):
+            prelimWave = newWave
+        else:
+            wave = newWave
+
+    #detect frequency
+    distances = []
+    if(wave == newWave and wave != "No signal"): #if we're confident about what the wave is 
+        #find peaks
+        peaks = [fuzzycompare(np.max(vals), vals[i], .5) for i in range(len(vals))]
+        #find distance between peaks
+        peak_indices = []
+        for i in range(1, len(vals)-1):
+            if( not peaks[i-1] and peaks[i]):
+                peak_indices.append(i)
+
+        for i in range(1, len(peak_indices)):
+            distances.append(peak_indices[i] - peak_indices[i-1])
+
+        #find frequency
+        freq = 1425 / np.mean(distances) #manally calibrated constant
+    else:
+        freq = 0
+        
+    os.system('clear')
+    print("Variance:", dydx_var)
+    print("Threshold:", var_threshold)
+    #print(dydx_same)
+    #print("zeroes", dydx_zeroes)
+    #print(vals)
+    print(wave)
+    print(freq)
+   # print(distances)
+
+#print(np.gradient(vals))
+
+
+
+    
---
+++ lab3/targetdriver.py
@@ -0,0 +1,44 @@
+#class abstracting away SPI and hit detection logic
+
+from time import sleep 
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
+class Target:
+
+    #threshold: value, in volts, of a detected hit
+    def __init__(self, threshold)
+        # create the spi bus
+        self.spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+        # create the cs (chip select)
+        self.cs = digitalio.DigitalInOut(board.D22)
+
+        # create the mcp object
+        self.mcp = MCP.MCP3008(spi, cs)
+
+        # create analog input channels
+        self.chan0 = AnalogIn(mcp, MCP.P0)
+        self.chan1 = AnalogIn(mcp, MCP.P1)
+
+        self.threshold = threshold
+
+    def detectHit():
+        hit0 = self.chan0.voltage > self.threshold
+        hit1 = self.chan1.voltage > self.threshold
+        return(hit0, hit1)
+
+
+if __name__ == "__main__":
+    while True:
+        hits = detectHit()
+        if hits[0]:
+            print("Target 0 hit!")
+        if hits[1]:
+            print("Target 1 hit")
+        
+
