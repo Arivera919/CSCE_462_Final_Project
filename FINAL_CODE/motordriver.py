42a6204e5d654f7117d0e75eb222ece9af6db24a
Arivera919 <Arivera919@outlook.com>
2024-12-02 20:31:37 -0600

    please please please

--- FINAL_CODE/motordriver.py
+++ FINAL_CODE/motordriver.py
@@ -3,8 +3,8 @@ from time import sleep
 
 class Motor:
 
-    self.CW = 1
-    self.CCW = 0
+    CW = 1
+    CCW = 0
 
     def __init__(self, DIR, STEP, goal):
         #setup GPIO pins for motor
@@ -16,7 +16,7 @@ class Motor:
         GPIO.setup(STEP, GPIO.OUT)
 
         #current direction of motor 1(CW) by default
-        self.currentDir = CW
+        self.currentDir = self.CW
 
         #number of steps the motor has taken
         self.steps = 0
@@ -32,27 +32,27 @@ class Motor:
         sleep(self.sleepTime)
         GPIO.output(self.step, GPIO.LOW)
 
-        if (self.currentDir == CW):
+        if (self.currentDir == self.CW):
             self.steps = self.steps + 1
-        elif (self.currentDir == CCW):
+        elif (self.currentDir == self.CCW):
             self.steps = self.steps - 1
 
     def change_dir(self, direction=-1, first_hit=False):
         sleep(0.5)
 
-        if (direction == CW):
-            GPIO.output(self.dir, CCW)
-            self.currentDir = CCW
-        elif (direction == CCW):
-            GPIO.output(self.dir, CW)
-            self.currentDir = CW
+        if (direction == self.CW):
+            GPIO.output(self.dir, self.CCW)
+            self.currentDir = self.CCW
+        elif (direction == self.CCW):
+            GPIO.output(self.dir, self.CW)
+            self.currentDir = self.CW
         else:
-            if (self.currentDir == CW):
-                GPIO.output(self.dir, CCW)
-                self.currentDir = CCW
-            elif (self.currentDir == CCW):
-                GPIO.output(self.dir, CW)
-                self.currentDir = CW
+            if (self.currentDir == self.CW):
+                GPIO.output(self.dir, self.CCW)
+                self.currentDir = self.CCW
+            elif (self.currentDir == self.CCW):
+                GPIO.output(self.dir, self.CW)
+                self.currentDir = self.CW
         
         if first_hit:
             self.sleepTime = self.sleepTime / 2
@@ -63,12 +63,12 @@ class Motor:
     def reset_motor(self):
         if(self.steps > 0):
             sleep(0.5)
-            GPIO.output(self.dir, CCW)
-            self.currentDir = CCW
+            GPIO.output(self.dir, self.CCW)
+            self.currentDir = self.CCW
         elif(self.steps < 0):
             sleep(0.5)
-            GPIO.output(self.dir, CW)
-            self.currentDir = CW
+            GPIO.output(self.dir, self.CW)
+            self.currentDir = self.CW
 
         while (self.step != 0):
             self.motor_step()
